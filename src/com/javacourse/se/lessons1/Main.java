c8ad7de17044c45ab252150122f2032191debbed
Admin <kr77.kot@gmail.com>
2021-08-21 03:56:12 +0300

    referances_ссылочные_типы_данных

--- src/com/javacourse/se/lessons1/Main.java
+++ src/com/javacourse/se/lessons1/Main.java
@@ -1,6 +1,7 @@
 package com.javacourse.se.lessons1;
 
 import com.javacourse.se.lessons3.Bus;
+import com.javacourse.se.lessons4.Referances;
 
 public class Main {
     public static void main(String[] args) {
---
+++ src/com/javacourse/se/lessons4/Referances.java
@@ -0,0 +1,20 @@
+package com.javacourse.se.lessons4;
+
+public class Referances {
+    String str = "Моя строка";
+    String str2 = new String("Моя строка");
+
+    Byte a;
+    Short b;
+    Integer c;
+    Long d;
+    Character e;
+    Float f;
+    Double g;
+    Boolean z;
+
+    public void foo() {
+        new String("Моя строка");
+    }
+}
+
