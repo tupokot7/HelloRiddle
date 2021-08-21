bc7ca28d11bc1bf5467d9f59d15fea938d15d804
Admin <kr77.kot@gmail.com>
2021-08-21 07:32:41 +0300

    switch_case_оператор

--- src/com/javacourse/se/lessons1/Main.java
+++ src/com/javacourse/se/lessons1/Main.java
@@ -4,10 +4,11 @@ import com.javacourse.se.lessons3.Bus;
 import com.javacourse.se.lessons4.Referances;
 import com.javacourse.se.lessons5.Operation;
 import com.javacourse.se.lessons6.ControlOperations;
+import com.javacourse.se.lessons7.OperatorSwitch;
 
 public class Main {
     public static void main(String[] args) {
-        ControlOperations co = new ControlOperations();
-        co.foo();
+        OperatorSwitch op = new OperatorSwitch();
+        op.foo();
     }
 }
---
+++ src/com/javacourse/se/lessons7/OperatorSwitch.java
@@ -0,0 +1,39 @@
+package com.javacourse.se.lessons7;
+
+public class OperatorSwitch {
+
+    final int VALUE = 2, FIRSTNAME = 1, SECONDLY = 4, THIRDLY = 3;
+//    int value = 2, firstValue = 1, secondValue = 2, thirdValue = 3;
+
+    public void foo() {
+         // Оператор множественного выбора.
+         // byte, short, int, char, String
+
+//        switch (48) {
+//            case 25:
+//                System.out.println(25);
+//                break;
+//            case 73:
+//                System.out.println(73);
+//                break;
+//            case 48:
+//                System.out.println(48);
+//                break;
+//            default:
+//                System.out.println("Not found");
+
+
+        switch (VALUE) {
+            case FIRSTNAME:
+                System.out.println(FIRSTNAME);
+                break;
+            case SECONDLY:
+                System.out.println(SECONDLY);
+                break;
+            case THIRDLY:
+                System.out.println(THIRDLY);
+            default:
+                System.out.println("Default!");
+        }
+    }
+}
