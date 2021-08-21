01f7366222911e1e9bc1c05fea3194e38659b61e
Admin <kr77.kot@gmail.com>
2021-08-21 07:59:39 +0300

    switch_case_оператор

--- src/com/javacourse/se/lessons7/OperatorSwitch.java
+++ src/com/javacourse/se/lessons7/OperatorSwitch.java
@@ -2,12 +2,12 @@ package com.javacourse.se.lessons7;
 
 public class OperatorSwitch {
 
-    final int VALUE = 1, FIRSTNAME = 1, SECONDLY = 4, THIRDLY = 3;
+//    final int VALUE = 1, FIRSTNAME = 1, SECONDLY = 4, THIRDLY = 3;
 //    int value = 2, firstValue = 1, secondValue = 2, thirdValue = 3;
 
     public void foo() {
-         // Оператор множественного выбора.
-         // byte, short, int, char, String
+        // Оператор множественного выбора.
+        // byte, short, int, char, String
 
 //        switch (48) {
 //            case 25:
@@ -23,17 +23,26 @@ public class OperatorSwitch {
 //                System.out.println("Not found");
 
 
-        switch (VALUE) {
-            case FIRSTNAME:
-                System.out.println(FIRSTNAME);
-                break;
-            case SECONDLY:
-                System.out.println(SECONDLY);
-                break;
-            case THIRDLY:
-                System.out.println(THIRDLY);
-            default:
-                System.out.println("Default!");
+        // лучше записывать так
+        switch (48) {
+            case 25 -> System.out.println(25);
+            case 73 -> System.out.println(73);
+            case 48 -> System.out.println(48);
+            default -> System.out.println("Not found");
+
+
+//                switch (VALUE) {       // Здесь работает с переменными по сути константами (поэтому все заглавные)
+//                    case FIRSTNAME:
+//                        System.out.println(FIRSTNAME);
+//                        break;
+//                    case SECONDLY:
+//                        System.out.println(SECONDLY);
+//                        break;
+//                    case THIRDLY:
+//                        System.out.println(THIRDLY);
+//                    default:
+//                        System.out.println("Default!");
+//                }
         }
     }
 }
